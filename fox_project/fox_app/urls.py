a952add085967ab38f8c1e9d06d367a4e8908d02
MANMOHAN771998 <manmohansingh771998@gmail.com>
2024-01-08 00:10:03 +0530

    all links are working and created contact us details saving functionality

--- fox_project/fox_app/urls.py
+++ fox_project/fox_app/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('e-books/', e_books_creation),
     path('contact/', contact),
     path('about/', about),
+    path('pricing/', pricing),
+    path('quality-assurance/', quality_assurance),
+    path('content-writing/', content_writing),
+    path('website-design/', website_design),
+    path('time-code-audio-video/', time_code_audio_video),
+    path('get-start/', get_start),
             ]
--- fox_project/fox_app/views.py
+++ fox_project/fox_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+from .models import *
 
 
 
@@ -11,7 +11,45 @@ def e_books_creation(request):
     return render(request, 'ebook.html')
 
 def contact(request):
+    if request.method == 'POST':
+        print(request.POST)
+        name  = request.POST.get('name')
+        email  = request.POST.get('email')
+        subject  = request.POST.get('subject')
+        message  = request.POST.get('message')
+        print(message,subject,email,name, sep='\n')
+        contactusers.objects.create(name=name,email=email,subject=subject,message=message)
+        return redirect('/')
     return render(request, 'contact.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+def quality_assurance(request):
+    return render(request, 'Quality-assurance.html')
+
+def content_writing(request):
+    return render(request, 'content-writing.html')
+
+def website_design(request):
+    return render(request, 'website-design.html')
+
+def time_code_audio_video(request):
+    return render(request, 'time-code-audio-video.html')
+
+def get_start(request):
+    return render(request, 'get-start.html')
+
+
+
+
+
+
+def view_contact_details(request):
+    pass
+
+
+
